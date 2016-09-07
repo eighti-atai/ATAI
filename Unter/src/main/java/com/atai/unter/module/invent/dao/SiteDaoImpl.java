3399712cde5393e6038bb6fa7d891f6ef004d712
yasarjun <kanchana2@gmail.com>
2016-09-07 21:24:16 +0530

    committing SiteDao

---
+++ Unter/src/main/java/com/atai/unter/module/invent/dao/SiteDaoImpl.java
@@ -0,0 +1,51 @@
+package com.atai.unter.module.invent.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.atai.unter.generic.interfaces.DataAcccessObject;
+import com.atai.unter.module.invent.model.Site;
+
+public class SiteDaoImpl implements DataAcccessObject<Site>
+{
+private SessionFactory sessionFactory;
+	
+	
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public void addData(Site p) {
+		Session session = sessionFactory.getCurrentSession();
+		session.persist(p);
+	}
+
+	public void updateData(Site p) {
+		Session session = sessionFactory.getCurrentSession();
+		session.update(p);
+		
+	}
+
+	public List<Site> listData() {
+		Session session = sessionFactory.getCurrentSession();
+		List<Site> sites = session.createQuery("from Site").list();
+		return sites;
+	}
+
+	public Site getDataById(String siteId) {
+		Session session = sessionFactory.getCurrentSession();
+		Site site = session.load(Site.class, new String(siteId));
+		return site;
+	}
+
+	public void removeData(String siteId) {
+		Session session = sessionFactory.getCurrentSession();
+		Site site = session.load(Site.class, new String(siteId));
+		if (site != null){
+			session.delete(site);
+		}
+	}
+}

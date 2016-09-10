a27da3eba9c216f3027df27dda9fd5c0583b7ffd
isumarasa <isumarasa@yahoo.com>
2016-09-10 17:05:22 +0530

    Add new Inventory Part v 1.0

--- Unter/src/main/java/com/atai/unter/module/invent/controller/InventoryPartController.java
+++ Unter/src/main/java/com/atai/unter/module/invent/controller/InventoryPartController.java
@@ -3,8 +3,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.atai.unter.module.invent.service.InventoryPartService;
 import com.atai.unter.module.invent.model.InventoryPart;
@@ -21,10 +23,23 @@ public class InventoryPartController {
 	}
 	
 	@RequestMapping(value="/inventoryPart", method=RequestMethod.GET)
-	public String listPersons(Model model)
+	public String listInvebtoryParts(Model model)
 	{
 		model.addAttribute("invPart", new InventoryPart());
 		model.addAttribute("listInvParts", this.invPartService.listInventoryParts());
+		return "inventoryPart";
+	}
+	
+	@RequestMapping(value="/showAddInventoryPart", method=RequestMethod.GET)
+	public ModelAndView showForm()
+	{
+		return new ModelAndView("handleInventoryPart", "addInventoryPart", new InventoryPart());
+	}
+	
+	@RequestMapping(value="/inventoryPart/add", method=RequestMethod.POST)
+	public String addInventoryPart(@ModelAttribute("addInventoryPart") InventoryPart inventoryPart)
+	{
+		invPartService.addInventoryPart(inventoryPart);
 		return "inventoryPart";
 	}
 }
---
+++ Unter/src/main/webapp/WEB-INF/views/handleInventoryPart.jsp
@@ -0,0 +1,35 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<html>
+<head>
+	<link href="webjars/bootstrap/3.3.7-1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+	<form:form class="form-horizontal" method="POST" modelAttribute="addInventoryPart" Action="inventoryPart/add">
+	<table class="table table-striped">
+	</table>
+		<tbody>
+			<tr>
+				<td><form:label path="invPartNo">Part No</form:label></td>
+				<td><form:input path="invPartNo"/></td>
+			</tr>
+			<tr>
+				<td><form:label path="description">Description</form:label></td>
+				<td><form:input path="description"/></td>
+			</tr>
+			<tr>
+				<td><form:label path="reorderLevel">Reorder Level</form:label></td>
+				<td><form:input path="reorderLevel"/>></td>
+			</tr>
+			<tr>
+				<td><form:label path="safetyStockLevel">Safety Stock Level</form:label></td>
+				<td><form:input path="safetyStockLevel"/></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="submit"></td>
+			</tr>
+		</tbody>
+	</form:form>
+</div>
+</body>
+</html>

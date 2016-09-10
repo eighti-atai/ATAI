9b52997dd2b5768c1d36d4a56f73e74b3a9ccf26
yasarjun <kanchana2@gmail.com>
2016-09-11 05:24:19 +0530

    InventoryLocation model files

--- Unter/src/main/resources/invent.sql
+++ Unter/src/main/resources/invent.sql
@@ -12,3 +12,16 @@ address_id 	INT SIGNED,
 phone_no_1  INT SIGNED,
 phone_no_2  INT SIGNED,
 FOREIGN KEY (address_id) REFERENCES address_tab(address_id));
+
+
+CREATE TABLE IF NOT EXISTS `unter`.`inventory_location_tab` (
+  `site_id` VARCHAR(5) NOT NULL,
+  `inv_location_id` VARCHAR(5) NOT NULL,
+  `inv_location_type` VARCHAR(10) NULL,
+  PRIMARY KEY (`site_id`, `inv_location_id`),
+  CONSTRAINT `site_id`
+    FOREIGN KEY (`site_id`)
+    REFERENCES `unter`.`site_tab` (`site_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;

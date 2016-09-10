4c686be68ae9a0e785395f4b259b0e5051ab81b2
yasarjun <kanchana2@gmail.com>
2016-09-11 05:24:19 +0530

    InventoryLocation data access object files

---
+++ Unter/src/main/java/com/atai/unter/module/invent/dao/InventoryLocationDaoImpl.java
@@ -0,0 +1,51 @@
+package com.atai.unter.module.invent.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.atai.unter.module.invent.model.InventoryLocation;
+import com.atai.unter.module.invent.model.InventoryLocationKey;
+
+public class InventoryLocationDaoImpl implements InventoryLocationDao{
+
+	private SessionFactory sessionFactory;
+	
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public void addData(InventoryLocation p) {
+		Session session = sessionFactory.getCurrentSession();
+		session.persist(p);
+	}
+
+	public void updateData(InventoryLocation p) {
+		Session session = sessionFactory.getCurrentSession();
+		session.update(p);
+	}
+
+	public List<InventoryLocation> listData() {
+		Session session = sessionFactory.getCurrentSession();
+		List<InventoryLocation> locations = session.createQuery("from InventoryLocation").list();
+		return locations;
+	}
+
+	public InventoryLocation getSiteById(InventoryLocationKey invLocationId) {
+		Session session = sessionFactory.getCurrentSession();
+		InventoryLocation invLocation = session.load(InventoryLocation.class, invLocationId);
+		return invLocation;
+	}
+
+	public void removeInventoryLocation(InventoryLocationKey invLocationId) {
+		Session session = sessionFactory.getCurrentSession();
+		InventoryLocation invLocation = session.load(InventoryLocation.class, invLocationId);
+		if (invLocation != null)
+		{
+			session.delete(invLocation);
+		}
+	}
+
+}

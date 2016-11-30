22f87f09864498e47674456b70fa61c1149fa873
isumarasa <isumarasa@yahoo.com>
2016-11-30 22:17:23 +0530

    Adding Site key to Inventory Part

--- Unter/src/main/java/com/atai/unter/module/invent/dao/InventoryPartDaoImpl.java
+++ Unter/src/main/java/com/atai/unter/module/invent/dao/InventoryPartDaoImpl.java
@@ -4,47 +4,72 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
+import com.atai.unter.generic.dao.AbstractDao;
+import com.atai.unter.module.invent.model.InventoryLocation;
+import com.atai.unter.module.invent.model.InventoryLocationKey;
 import com.atai.unter.module.invent.model.InventoryPart;
+import com.atai.unter.module.invent.model.InventoryPartKey;
 
-@Repository
-public class InventoryPartDaoImpl implements InventoryPartDao {
+public class InventoryPartDaoImpl extends AbstractDao<InventoryPartKey, InventoryPart> implements InventoryPartDao {
 
-	@Autowired
 	private SessionFactory sessionFactory;
+	
+	public void addData(InventoryPart p) {
+		Session session  = sessionFactory.getCurrentSession();
+		session.persist(p);
+
+	}
+
+	public void updateData(InventoryPart p) {
+		Session session = sessionFactory.getCurrentSession();
+		session.update(p);
+	}
+
+	public List<InventoryPart> listData() {
+		Session session = sessionFactory.getCurrentSession();
+		List<InventoryPart> invParts = session.createQuery("from InventoryPart").list();
+		return invParts;
+	}
+
+	public InventoryPart getInventoryPartById(InventoryPartKey invPartId) {
+		Session session = sessionFactory.getCurrentSession();
+		InventoryPart invPart = session.load(InventoryPart.class, invPartId);
+		return invPart;
+	}
+
+	public void remove(InventoryPartKey invPartId) {
+		Session session = sessionFactory.getCurrentSession();
+		InventoryPart invPart = session.load(InventoryPart.class, invPartId);
+		if (invPart != null)
+		{
+			session.delete(invPart);
+		}
+	}
+
+	public InventoryPart getInventoryPartByObjid(String objid) {
+		Session session = this.sessionFactory.getCurrentSession();	
+		List<InventoryPart> invPartList = session.createQuery("from InventoryPart where objid = '" + objid+"'").list();
+		InventoryPart invPart = invPartList.get(0);
+		
+		return invPart;
+	}
+
+	public void removeInventoryPart(InventoryPartKey invPartId) {
+		Session session = sessionFactory.getCurrentSession();
+		InventoryPart invPart = session.load(InventoryPart.class, invPartId);
+		if (invPart != null)
+		{
+			session.delete(invPart);
+		}
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
-	}
-
-	public void addInventoryPart(InventoryPart invPart) {
-		Session session = this.sessionFactory.getCurrentSession();
-		session.persist(invPart);
-	}
-
-	public void updateInventoryPart(InventoryPart invPart) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public List<InventoryPart> listInventoryParts() {
-		Session session = this.sessionFactory.getCurrentSession();
-		List<InventoryPart> invPartList = session.createQuery("from InventoryPart").list();
-		return invPartList;
-	}
-
-	public InventoryPart getInventoryPartByNo(String invPartNo) {
-		Session session = this.sessionFactory.getCurrentSession();
-		// do not use load https://www.mkyong.com/hibernate/different-between-session-get-and-session-load/
-		InventoryPart invPart2 = (InventoryPart)session.get(InventoryPart.class, new String(invPartNo));
-		return invPart2;
-	}
-
-	public void removeInventoryPart(String sPartNo) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
